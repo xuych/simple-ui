fe422c334f7574b98d032624d98dc1a19245ee5a
xuych <s.w.a.l.l.o.w@qq.com>
2020-08-21 12:30:08 +0800

    fix:basic config done

--- examples/main.js
+++ examples/main.js
@@ -1,3 +1,4 @@
+import './public-path'
 import Vue from 'vue'
 import App from './App.vue'
 import routes from './router'
@@ -12,16 +13,18 @@ let instance = null
 
 function render() {
   router = new VueRouter({
-    base: window.__POWERED_BY_QIANKUN__ ? '/simple-ui' : '/',
-    mode: 'history',
     routes,
   })
-
   instance = new Vue({
     router,
-
+    data() {
+      return {
+        parentRouter: parent.router,
+        parentVuex: parent.store,
+      }
+    },
     render: (h) => h(App),
-  }).$mount('#appVueHistory')
+  }).$mount('#simple-ui')
 }
 
 if (!window.__POWERED_BY_QIANKUN__) {
@@ -29,20 +32,23 @@ if (!window.__POWERED_BY_QIANKUN__) {
 }
 //测试全局变量污染
 window.a = 1
+console.log('window.a', window.a)
+
 export async function bootstrap() {
   console.log('vue app bootstraped')
 }
 
 export async function mount(props) {
-  console.log('props from main framework', props)
-  render()
   // 测试一下 body 的事件，不会被沙箱移除
-  // document.body.addEventListener('click', e => console.log('document.body.addEventListener'))
-  // document.body.onclick = e => console.log('document.body.addEventListener')
+  // document.body.addEventListener('click', (e) => console.log('document.body.addEventListener'))
+  // document.body.onclick = (e) => console.log('document.body.addEventListener')
+  console.log('props from main framework', props.data)
+  render(props.data)
 }
 
 export async function unmount() {
   instance.$destroy()
+  // instance.$el.innerHTML = ''
   instance = null
   router = null
 }
